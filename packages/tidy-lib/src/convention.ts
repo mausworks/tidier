7f66221c31923aaeb1b261f3e8b8aef512ad55cf
mausworks <rasmuswennerstrom@gmail.com>
2021-07-25 04:11:12 +0200

    fix: some generalization of conventions

--- packages/tidy-lib/src/convention.ts
+++ packages/tidy-lib/src/convention.ts
@@ -14,15 +14,6 @@ export const generalCasings = [
   "UPPER_SNAKE_CASE",
   "sPoNGEcAsE",
 ] as const;
-/** Describes the supported extension casings. */
-export const extensionCasings = ["p", "lc", "UC", "Tc"] as const;
-
-/** A general casing can be applied to any fragment of the name. */
-export type GeneralCasing = typeof generalCasings[number];
-/** An extension casing can only be applied to the extension of the name (the last fragment). */
-export type ExtensionCasing = typeof extensionCasings[number];
-/** Denotes that this can be either a general casing, or an extension casing. */
-export type EitherCasing = GeneralCasing | ExtensionCasing;
 
 /** Aliases that can be used for general casings in configs etc ... */
 export const generalAliases = {
@@ -35,21 +26,31 @@ export const generalAliases = {
   "UPPER-KEBAB-CASE": "COBOL-CASE",
 } as const;
 
+/** Describes the supported extension casings. */
+export const extensionCasings = ["p", "lc", "UC", "Tc"] as const;
+
+/** A general casing can be applied to any fragment of the name. */
+export type GeneralCasing = typeof generalCasings[number];
+/** An extension casing can only be applied to the extension of the name (the last fragment). */
+export type ExtensionCasing = typeof extensionCasings[number];
+/** Denotes that this can be either a general casing, or an extension casing. */
+export type EitherCasing = GeneralCasing | ExtensionCasing;
 /** An alias for a general casing */
 export type GeneralAlias = keyof typeof generalAliases;
-
 /** Describes how each fragment of a name should be formatted. */
-export type NameFormat =
-  | (GeneralCasing | GeneralAlias)[]
-  | [...(GeneralCasing | GeneralAlias)[], ExtensionCasing];
+export type NameFormat = (GeneralCasing | GeneralAlias)[];
 
 /** Describes which files should apply what name format. */
 export interface NameConvention {
-  glob: string;
-  format: NameFormat;
+  /** The glob pattern to match. */
+  readonly glob: string;
+  /**
+   * The format of the name to use for the
+   * files or folders that match the glob.
+   */
+  readonly format: NameFormat;
 }
 
 /** Resolve the casing from a potential alias into the canon casing name. */
-export const resolveCasing = (
-  casingOrAlias: GeneralAlias | GeneralCasing | ExtensionCasing
-) => generalAliases[casingOrAlias as GeneralAlias] ?? casingOrAlias;
+export const resolveCasing = (casingOrAlias: GeneralAlias | EitherCasing) =>
+  generalAliases[casingOrAlias as GeneralAlias] ?? casingOrAlias;
