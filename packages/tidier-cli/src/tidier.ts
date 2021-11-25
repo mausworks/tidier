9ce16000a7926d406cc30d621c061b98058035cf
mausworks <rasmuswennerstrom@gmail.com>
2021-11-25 20:55:22 +0100

    feat: remove broken watch functionality

--- packages/tidier-cli/src/tidier.ts
+++ packages/tidier-cli/src/tidier.ts
@@ -1,16 +1,14 @@
 import { check } from "./check";
 import { TidierCLIOptions } from "./options";
 import { projectFromOptions } from "./project";
-import { watchProject } from "./watch";
 import { write } from "./write";
 
 export async function tidier(options: TidierCLIOptions) {
   const project = await projectFromOptions(options);
 
-  if (options.watch) {
-    await watchProject(project, options);
-  } else if (options.check) {
-    await check(project);
+  if (options.check) {
+    const exitCode = await check(project);
+    process.exit(exitCode);
   } else if (options.write) {
     await write(project);
   }
